476dfaad2cd6aafc7e3cc37af24c13d3e7ccfed1
kTrzcinskii <kt.trzcinski@gmail.com>
2023-02-16 19:38:26 +0100

    main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
+#include "PasswordGenerator.h"
+#include <stdexcept>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "WELCOME TO PASSWORD GENERATOR!\n";
+	try
+	{
+		password_generator::PasswordGenerator pg(argc, argv);
+		pg.generate();
+	}
+	catch (const std::invalid_argument&)
+	{
+		//this exception is thrown when somehting was wrong with arguments
+		//the message is already displayed and right here we only want to exit the program
+		std::cout << "\nTry running the program again with valid arguments!\n";
+		exit(1);
+	}
 	return 0;
 }
